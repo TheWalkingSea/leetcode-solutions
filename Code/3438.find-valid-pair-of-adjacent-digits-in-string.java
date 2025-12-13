ecd12b0a7cbcf4564b27940ae748e95049594458
Austin McCallister <91487740+TheWalkingSea@users.noreply.github.com>
2025-12-13 11:12:50 -0500

    Finished problem 3438

--- Code/3438.find-valid-pair-of-adjacent-digits-in-string.java
+++ Code/3438.find-valid-pair-of-adjacent-digits-in-string.java
@@ -57,10 +57,33 @@
 // 	2 <= s.length <= 100
 // 	s only consists of digits from '1' to '9'.
 // 
- 
+import java.util.*;
 
 class Solution {
     public String findValidPair(String s) {
-        
+        Map<Integer, Integer> occurences = new HashMap<>(s.length());
+
+        for (Character curr : s.toCharArray()) {
+            Integer currInt = Integer.valueOf(String.valueOf(curr));
+            occurences.put(currInt, occurences.getOrDefault(currInt, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            Character s1 = s.charAt(i);
+            Character s2 = s.charAt(i + 1);
+
+            if (s1.equals(s2)) {
+                continue;
+            }
+
+            Integer s1val = Integer.valueOf(String.valueOf(s1));
+            Integer s2val = Integer.valueOf(String.valueOf(s2));
+
+            if (occurences.get(s1val).compareTo(s1val) == 0 && occurences.get(s2val).compareTo(s2val) == 0) {
+                return String.valueOf(s1) + s2;
+            }
+        }
+
+        return "";        
     }
 }
