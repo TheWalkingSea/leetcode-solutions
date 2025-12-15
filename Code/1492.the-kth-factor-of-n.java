99f7ea399d3d27d304b8a74a34b9e274d0c77c1c
Austin McCallister <91487740+TheWalkingSea@users.noreply.github.com>
2025-12-15 11:49:52 -0500

    Completed MEDIUM problem 1492

--- Code/1492.the-kth-factor-of-n.java
+++ Code/1492.the-kth-factor-of-n.java
@@ -45,6 +45,15 @@
 
 class Solution {
     public int kthFactor(int n, int k) {
-        
+        int numfactor = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                numfactor++;
+                if (numfactor == k) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 }
